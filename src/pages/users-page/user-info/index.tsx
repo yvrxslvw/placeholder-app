1dd1ecd6972f854967b1d3a96b491cdd2a2e59f0
yvrxslvw <yvrxslvv@gmail.com>
2023-10-05 08:52:24 +0200

    Add user info title

--- src/pages/users-page/user-info/index.tsx
+++ src/pages/users-page/user-info/index.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
-import { Container } from 'shared';
+import s from './style.module.scss';
+import { Container, Heading } from 'shared';
 import { UserInfo } from 'widgets';
 
 export const UserInfoPage: FC = () => {
 	return (
 		<Container>
+			<Heading level={2} className={s.title}>
+				User info:
+			</Heading>
 			<UserInfo />
 		</Container>
 	);
