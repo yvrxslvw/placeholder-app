ef2c2a503570b74baa7505c0066586da9735d679
yvrxslvw <yvrxslvv@gmail.com>
2023-10-05 08:50:38 +0200

    Add title to the user list page

--- src/pages/users-page/users-list/index.tsx
+++ src/pages/users-page/users-list/index.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
-import { Container } from 'shared';
+import s from './style.module.scss';
+import { Container, Heading } from 'shared';
 import { UserList } from 'widgets';
 
 export const UsersPage: FC = () => {
 	return (
 		<Container>
+			<Heading level={2} className={s.title}>
+				User list:
+			</Heading>
 			<UserList />
 		</Container>
 	);
